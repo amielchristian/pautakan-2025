4ad2ae60e0780bfa796e6c90d67aa719f69fc7c1
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-03-09 20:19:18 +0800

    Copied icons and components from latest

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,24 +1,42 @@
-function Difficulty() {
+function CategoryDisplay(props: any) {
+  const content: string = props.content;
   return (
-    <div className='sharp-edge-box bg-black w-auto h-111/1280 [--bottom-left:20px] [--top-right:20px]'></div>
+    <div
+      className='sharp-edge-box bg-black w-auto h-111/1280 [--bottom-left:20px] [--top-right:20px]
+    [--img:linear-gradient(#222,#111)] font-[DS-Digital]
+    text-4xl flex items-center justify-center
+    [--border-width:2px] border-[2px]
+    [--border-color:#f00] border-[#f00]'
+    >
+      {content}
+    </div>
   );
 }
 
-export default function Sidebar() {
+export default function Sidebar(props: any) {
+  const colleges: string[] = props.colleges || [
+    './icons/AB.png',
+    './icons/ACC.png',
+    './icons/ARKI.png',
+  ];
+  const difficulty: string = props.difficulty || 'Easy';
+  const category: string = props.category || 'Individual';
   return (
     <>
       <div
-        className='sharp-edge-box text-white text-4xl
+        className='sharp-edge-box text-white text-4xl font-[Starter] font-bold
       flex items-center justify-center
       w-auto h-240/1280 [--all:20px] grid-pattern'
       >
         Pautakan 2025
       </div>
-      <div className='sharp-edge-box w-auto h-460/1280 [--all:20px] grid-pattern'></div>
-      <div className='sharp-edge-box bg-black w-auto h-111/1280 [--bottom-left:20px] [--top-right:20px]'></div>
-      <Difficulty />
-      <Difficulty />
-      <Difficulty />
+      <div className='sharp-edge-box w-auto h-460/1280 [--all:20px] grid-pattern'>
+        {colleges.map((x) => (
+          <img src={x} />
+        ))}
+      </div>
+      <CategoryDisplay content={category} />
+      <CategoryDisplay content={difficulty} />
     </>
   );
 }
